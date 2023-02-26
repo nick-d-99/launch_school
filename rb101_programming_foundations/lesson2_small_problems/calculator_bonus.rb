c9dfe58376e70ea78a5a7332f3dad9082a4cfcab
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-02-25 19:11:55 -0500

    Completed Bonus Calculator assignments. Finish a couple days ago - forgot to commit

--- rb101_programming_foundations/lesson2_small_problems/calculator_bonus.rb
+++ rb101_programming_foundations/lesson2_small_problems/calculator_bonus.rb
@@ -10,8 +10,22 @@ Build a command line calculator program that does the following:
 
 =end
 
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts "=> #{message}"
+
+  # path = './configuration.yml'
+  # output = "=> #{message}"
+  # # puts "=> #{message}"
+  # puts output
+  # # puts File.exists?(path)
+  # if File.exist?(path)
+  #   File.write(path, output + "\n", mode: 'a')
+  # else
+  #   File.write(path, output + "\n")
+  # end
 end
 
 def operation_to_message(op)
@@ -42,9 +56,11 @@ end
 
 def get_number(reattempt = false)
   if reattempt
-    prompt('Please enter a valid number.')
+    # prompt('Please enter a valid number.')
+    prompt(MESSAGES['valid_number'])
   else
-    prompt('Please enter a number.')
+    # prompt('Please enter a number.')
+    prompt(MESSAGES['enter_number'])
   end
   num_string = gets.chomp
   num_string
@@ -74,17 +90,25 @@ def number?(num)
   end
 end
 
-loop do
-  prompt('Welcome to Calculator! Enter your name:')
-  name = get_name
-  prompt("Hi #{name}!")
+prompt(MESSAGES['welcome'])
+name = get_name
+prompt("Hi #{name}!")
 
+loop do
+  # prompt('Welcome to Calculator! Enter your name:')
+  # prompt(MESSAGES['welcome'])
+  # name = get_name
+  # prompt("Hi #{name}!")
+  # prompt(MESSAGES['name_respond'] )
+
+  # get numbers
   num1_string = get_number
   num1 = number?(num1_string)
 
   num2_string = get_number
   num2 = number?(num2_string)
 
+  # get operation
   operator_prompt = <<-MSG 
   Please enter the number corresponding with the operation you would like to execute.
       1. Add
@@ -104,35 +128,46 @@ loop do
   op = gets.chomp
 
   loop do
+    # loop until user enters value that is in the op array
     if operations.include?(op)
       break
     else
-      prompt('Please enter a valid operation.')
+      # prompt('Please enter a valid operation.')
+      prompt(MESSAGES['valid_operator'])
       op = gets.chomp
     end
   end
 
-  prompt("#{operation_to_message(op)} the two numbers...")
+  # prompt("#{operation_to_message(op)} the two numbers...")
 
   case op
   when '1'
-    prompt('Addition selected!')
+    # prompt('Addition selected!')
+    prompt(MESSAGES['addition'])
     puts num1 + num2
   when '2'
-    prompt('Subtraction selected!')
+    # prompt('Subtraction selected!')
+    prompt(MESSAGES['subtraction'])
     puts num1 - num2
   when '3'
-    prompt('Multiplication Selected!')
+    # prompt('Multiplication Selected!')
+    prompt(MESSAGES['multiplication'])
     puts num1 * num2
   when '4'
-    prompt('Division selected!')
+    # prompt('Division selected!')
+    prompt(MESSAGES['division'])
     puts num1.to_f / num2.to_f
   end
 
-  prompt('Would you like to continue?')
-  prompt('Y/N')
+  # prompt('Would you like to continue?')
+  prompt(MESSAGES['continue?'])
+  # prompt('Y/N')
+  prompt(MESSAGES['continue_options'])
   cont = gets.chomp
   break if cont.downcase == 'n'
   # end
-  prompt('Thanks for using Calculator!')
+  # prompt('Thanks for using Calculator!')
+  # prompt(MESSAGES['parting'])
 end
+
+prompt(MESSAGES['parting'])
