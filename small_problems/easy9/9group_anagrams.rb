71757b57c21d819e3a0e43f334f0461caa9d6544
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-06-05 18:49:06 -0400

    Stumped by this one and too tired to continue working. My inital idea is to create a pool of letters for each word, then compare each other word to see if each letter is within the pool. Where I am getting stumped is I need a way to store these pools. How do I know what the demo pool is or that  dome is within the same pool? Dictionary maybe? Sleep on it and see what I come up with.

---
+++ small_problems/easy9/9group_anagrams.rb
@@ -0,0 +1,14 @@
+# Write a program that prints out groups of words that are anagrams. Anagrams are words that have the same exact letters in them but in a different order. Your output should look something like this:
+WORDS =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+  'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+  'flow', 'neon']
+
+def anagrams
+  pools = 
+  WORDS.each do |word|
+  end
+end
+
+anagrams
+
+# 
