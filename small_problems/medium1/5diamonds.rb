7e22189da0761e10b89a9cd0a99688f7d22936b7
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-06-15 15:20:15 -0400

    Solved Diamonds in Medium 1.

--- small_problems/medium1/5diamonds.rb
+++ small_problems/medium1/5diamonds.rb
@@ -2,44 +2,52 @@
 # assume n will always be odd
 
 # takes the max characters and how many stars are required and populates a row
+def display(arr)
+  arr.each { |row| puts row }
+end
+
 def generate_row(max, stars)
-  max.times do |i|
-    # 
+  half = max / 2
+  half_stars = stars / 2
+
+  star_count = 0
+  right = []
+  until star_count == half_stars
+    right << '*'
+    star_count += 1
   end
+
+  right << ' ' until right.size == half
+
+  left = right.reverse
+  left << '*'
+  row = left + right
 end
 # need to figure out how to center a row
 # how to put stars into row and have them centered with even spaces on each side
 # could try cutting in half and padding at end?
+# number of stars increases by 2 each row
 
-def generate_half(n, mid)
-  # half = []
-  # n.times do |i|
-  #   curr = ''
-  #   if i < mid
-  #     curr << ' '
-  #   elsif i == mid
-  #     curr << '*'
-  #   else
-  #     curr << ' '
-  #   end
-  #   half << curr
-  # end
-  # puts half
-  generate_row(n, 3)
+def diamond(max)
+  top = []
+  bottom = []
+  sol = []
+
+  1.step(max, 2) do |i|
+    top << generate_row(max, i) if i < max
+    bottom << generate_row(max, max - i)
+  end
+
+  arrs = top + bottom
+  arrs.each { |row| sol << row.join }
+
+  display(sol)
 end
 # once row generation works need to write algorithm to put rows together
+# number of stars starts at 1, then increases by 2 each row until middle
 
-def diamond(n)
-  sol = []
-  mid = n / 2
-  mid_row = ['*' * n]
-
-  top = generate_half(n, mid)
-  # bottom = top but flipped
-end
-
-# diamond(1)
-# diamond(3)
+diamond(1)
+diamond(3)
 diamond(9)
 
 # need to find max length of a row
