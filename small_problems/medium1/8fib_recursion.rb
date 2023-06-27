df3b7d8a98746298d909bc5e96c52d27599dc98f
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-06-27 16:42:30 -0400

    Completed exercise 8.

--- small_problems/medium1/8fib_recursion.rb
+++ small_problems/medium1/8fib_recursion.rb
@@ -1,9 +1,21 @@
 def fib(num)
-  return 1 if num == 1
+  return 1 if num <= 2
 
-  num + fib(num - 1)
+  fib(num - 1) + fib(num - 2)
 end
 
 p fib(1)
 p fib(2)
 p fib(3)
+p fib(4)
+p fib(5)
+p fib(12)
+p fib(20)
+
+# p fib(1) == 1
+# p fib(2) == 1
+# p fib(3) == 2
+# p fib(4) == 3
+# p fib(5) == 5
+# p fib(12) == 144
+# p fib(20) == 6765
