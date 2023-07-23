04f0b68bd9ee0c745e6806dff7b35f469d2ffdb2
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-07-23 11:51:55 -0400

    Got a good start on exercise 2.

---
+++ small_problems/medium2/2abcs.rb
@@ -0,0 +1,49 @@
+# A collection of spelling blocks has two letters per block, as shown in this list:
+
+# B:O   X:K   D:Q   C:P   N:A
+# G:T   R:E   F:S   J:W   H:U
+# V:I   L:Y   Z:M
+
+# This limits the words you can spell with the blocks to just those words that do not use both letters from any given block. Each letter in each block can only be used once.
+
+# Write a method that returns true if the word passed in as an argument can be spelled from this set of blocks, false otherwise.
+
+BLOCKS = {
+  B: 'O',
+  X: 'K',
+  D: 'Q',
+  C: 'P',
+  N: 'A',
+  G: 'T',
+  R: 'E',
+  F: 'S',
+  J: 'W',
+  H: 'U',
+  V: 'I',
+  L: 'Y',
+  Z: 'M'
+}
+
+def block_word?(word)
+  banned = []
+  word.each_char do |char|
+    if banned.include? char
+      false
+    else
+      banned << char
+      banned << BLOCKS[char.to_sym]
+    end
+  end
+  true
+end
+
+p block_word?('BATCH') == true
+p block_word?('BUTCH') == false
+p block_word?('jest') == true
+p block_word?('moz') == false
+
+# need to accout for values
+# right now the program added looks at keys
+# for example, if the letter M is used, Z cannot be in the word
+# however the way I wrote it, it doesn't add Z to the array of banned letters
+# because M is a VALUE not a KEY
