e3fd3e6662ece2c764f90859f13308dd2c012a3b
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-07-24 16:57:00 -0400

    Completed exercise 8. This one was interesting. Given a number, the function must find the next number that is a multiple of 7, an odd number, and has no repeating digits (i.e. 100). I was able to provide a solution that fulfilled all these requirements however the book offered some very interesting optimizations I just want to note in hope of remembering or seeing later. For one, I increment by 7 everytime to maintain the value as a multiple of 7. An optimization would be to increment by 14 to maintain its multiplicity as well as maintain an odd number. This eliminates half of the useless numbers generated (the multiples of 7 that are even) as well as saves time and resources (albeit not a lot) on checking if the value is odd.  Just thought it was worth noting. Also worth noting that in order for the val += 14 optimization to work, the starting number must be odd as well. In some cases, my code's starting point is an even number. A simple check to ensure the starting number is odd would fix all this.

--- small_problems/medium2/8next_ft_num.rb
+++ small_problems/medium2/8next_ft_num.rb
@@ -32,6 +32,7 @@ def featured(num)
     end
     # val += 7
     val += 14 # book solution includes that increasing by 14 maintains an odd number
+    val += 7 # book solution includes that increasing by 14 maintains an odd number
     # this would eliminate half the numbers generated for val as well as save time and
     # resources on checking if the val was odd
   end
@@ -46,3 +47,18 @@ p featured(999_999) == 1_023_547
 p featured(999_999_987) == 1_023_456_987
 
 featured(9_999_999_999) # -> There is no possible number that fulfills those requirements
+
+# This one was interesting. Given a number, the function must
+# find the next number that is a multiple of 7, an odd number, and has no repeating
+# digits (i.e. 100). I was able to provide a solution that fulfilled all these
+# requirements however the book offered some very interesting optimizations I just
+# want to note in hope of remembering or seeing later. For one, I increment by 7
+# evertime to maintain the value as a multiple of 7. An optimization would be to
+# increment by 14 to maintain its multiplicity as well as maintain an odd number.
+# This eliminates half of the useless numbers generated (the multiples of 7 that are
+# even) as well as saves time and resources (albeit not a lot) on checking if the
+# value is odd. Just thought it was worth noting.
+
+# Worth noting that in order for the val += 14 optimization to work, the starting
+# number must be odd as well. In some cases, my code's starting point is an even
+# number. A simple check to ensure the starting number is odd would fix all this.
