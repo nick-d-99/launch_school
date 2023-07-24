53cb16be403ef797c5351fa2c98dc3cdef1d57d9
Nick DiBlasio <nicholas.diblasio@gmail.com>
2023-07-24 16:07:15 -0400

    Completed exercise 7.

---
+++ small_problems/medium2/7unlucky_days.rb
@@ -0,0 +1,20 @@
+# Write a method that returns the number of Friday the 13ths in the year given by an argument. You may assume that the year is greater than 1752 (when the United Kingdom adopted the modern Gregorian Calendar) and that it will remain in use for the foreseeable future.
+require 'date'
+
+def friday_13th(year)
+  count = 0
+  day = '13'
+  year.to_s
+  (1..12).each do |month|
+    month.to_s
+    str = "#{year.to_s}-#{month}-#{day}"
+    date = Date.parse(str)
+    count += 1 if date.wday == 5
+  end
+  count
+end
+
+p friday_13th(2023) == 2
+p friday_13th(2015) == 3
+p friday_13th(1986) == 1
+p friday_13th(2019) == 2
