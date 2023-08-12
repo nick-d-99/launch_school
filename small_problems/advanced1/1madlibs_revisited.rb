05f0411954431cf2835f6fb566a5ee18ec603a77
nick-d <nicholas.diblasio@gmail.com>
2023-08-12 11:24:16 -0400

    Completed 1st exercise of advanced 1. Intentionally did not overwrite text file with the madlib output but printed instead. Capability to overwrite is commented out for ease of testing. To implement overwriting original text file it might be worth doing all on one open instead of opening the file, saving each line, modifying each line, then overwriting the original. Definitely a way to open the file, work on it and close it without opening twice.

--- small_problems/advanced1/1madlibs_revisited.rb
+++ small_problems/advanced1/1madlibs_revisited.rb
@@ -1,1 +1,73 @@
-puts 'hello world (from a mac!)'
+# Let's build another program using madlibs. We made a program like this in the easy exercises. This time, the requirements are a bit different.
+
+# Make a madlibs program that reads in some text from a text file that you have created, and then plugs in a selection of randomized nouns, verbs, adjectives, and adverbs into that text and prints it. You can build your lists of nouns, verbs, adjectives, and adverbs directly into your program, but the text data should come from a file or other external source. Your program should read this text, and for each line, it should place random words of the appropriate types into the text, and print the result.
+
+NOUNS = ['shed', 'dehumidifier', 'pool noodle']
+ADJ = ['gross', 'argumentative', 'hairy']
+VERB = ['oxidizes', 'jumps', 'gathers']
+ADVERB = ['quickly', 'happily', 'supportively']
+
+
+def get_noun
+  NOUNS.sample
+end
+
+def get_adj
+  ADJ.sample
+end
+
+def get_verb
+  VERB.sample
+end
+
+def get_adverb
+  ADVERB.sample
+end
+
+def replace(arr, part, loc)
+  case part
+  when '%{noun}'
+    arr[loc] = get_noun
+  when '%{adjective}'
+    arr[loc] = get_adj
+  when '%{verb}'
+    arr[loc] = get_verb
+  when '%{adverb}'
+    arr[loc] = get_adverb
+  end
+  # p arr
+  # p part
+  # p loc
+end
+
+path_str = "madlibs.txt"
+# file = File.open("madlibs.txt")
+word_arr = []
+# File.foreach(path_str) do |line|
+#     # line.split.each { |word| word_arr << word }
+#     word_arr << line
+# end
+File.readlines(path_str).each { |line| word_arr << line }
+# word_arr.each_with_index do |line,  idx|
+sol = []
+word_arr.each do |line|
+  line_arr = line.split
+  line_arr.each_with_index do |word, idx|
+    if word.start_with?('%')
+      until word.end_with?('}')
+        word.chop!
+      end
+      replace(line_arr, word,idx)
+    end
+  end
+  # sol << line_arr
+  sol << line_arr.join(' ')
+end
+
+puts sol
+
+# this code will overwrite the file with the replacement parts of speech
+
+# File.open(path_str, 'w+') do |f|
+#   sol.each { |line| f.write(line+"\n") }
+# end
