04cb90831bcb3f6be7376d201dc8d0627dbca5ad
nick-d <nicholas.diblasio@gmail.com>
2023-08-13 18:40:59 -0400

    Completed exercise 3.

---
+++ small_problems/advanced1/3transpose.rb
@@ -0,0 +1,24 @@
+# Write a method that takes a 3 x 3 matrix in Array of Arrays format and returns the transpose of the original matrix. Note that there is a Array#transpose method that does this -- you may not use it for this exercise. You also are not allowed to use the Matrix class from the standard library. Your task is to do this yourself.
+
+# Take care not to modify the original matrix: you must produce a new matrix and leave the original matrix unchanged.
+
+def transpose(mat)
+  sol = [[], [], []]
+  mat.each_with_index do |arr, arr_idx|
+    arr.each_with_index do |val, idx|
+      sol[arr_idx][idx] = mat[idx][arr_idx]
+    end
+  end
+  sol
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
