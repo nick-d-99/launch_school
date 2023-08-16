5278873387aee51110efdc02a99430ba8e47efe5
nick-d <nicholas.diblasio@gmail.com>
2023-08-16 18:49:52 -0400

    Completed Medium 1.

---
+++ rb101_programming_foundations/lesson3_practice_problems/medium1/question5.rb
@@ -0,0 +1,26 @@
+# Alyssa asked Ben to write up a basic implementation of a Fibonacci calculator. A user passes in two numbers,
+# and the calculator will keep computing the sequence until some limit is reached.
+
+# Ben coded up this implementation but complained that as soon as he ran it, he got an error. Something about 
+# the limit variable. What's wrong with the code?
+
+limit = 15
+
+def fib(first_num, second_num)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1)
+puts "result is #{result}"
+
+# The limit variable is not within the method's scope.
+
+
+# How would you fix this so that it works?
+
+# I would add another input variable to pass in the limit variable
